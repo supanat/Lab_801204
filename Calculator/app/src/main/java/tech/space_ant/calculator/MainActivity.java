29871067e9205a368667aac5c784c6e6b2112e56
Supanat <supanat.ji@gmail.com>
2019-02-18 10:31:51 +0700

    calculator

---
+++ Calculator/app/src/main/java/tech/space_ant/calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package tech.space_ant.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
